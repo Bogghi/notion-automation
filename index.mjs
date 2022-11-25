ddc785e56be246c89b9a1dc40b6ddf01eb310a00
matteo.borghi <matteosioneborghi@gmail.com>
2022-11-25 23:53:59 +0100

    implemented a proper data convertion from notion

--- index.mjs
+++ index.mjs
@@ -10,6 +10,9 @@ dotenv.config()
 
 const notion = new Client({auth: process.env.NOTION_KEY})
 const databaseId = process.env.NOTION_DATABASE_ID
+const SCOPES = ['https://www.googleapis.com/auth/calendar']
+const TOKEN_PATH = path.join(process.cwd(), 'token.json')
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json') 
 
 const getSceduledEvents = async () => {
     let response = {}
@@ -31,17 +34,62 @@ const getSceduledEvents = async () => {
     return response;
 }
 
-const preperaDataForCalendar = () => getSceduledEvents().then((resolve) => {
+const preparateDataForCalendar = () => getSceduledEvents().then((resolve) => {
     let scheduledEvenets = []
 
     resolve.results.forEach((event, index) => {
+        let startStr = event.properties.Giorno.date.start,
+            endStr = event.properties.Giorno.date.end,
+            start = {},
+            end = {},
+            date,
+            dateTime,
+            timeZone
+
+        if(startStr !== null && endStr !== null)
+            timeZone = 'CET'
+        else 
+            timeZone = null
+        
+        if(startStr !== null && startStr.length > 10){
+            date = startStr.substr(0,9)
+            dateTime = startStr.substr(0, startStr.indexOf('+'))
+        }else {
+            date = startStr
+            dateTime = null
+        }
+
+        start = {
+            date: date,
+            dateTime: dateTime,
+            timeZone: timeZone
+        }
+
+        if(endStr !== null && endStr.length > 10){
+            date = endStr.substr(0,9)
+            dateTime = endStr.substr(0, endStr.indexOf('+'))
+        }else {
+            date = endStr
+            dateTime = null
+        }
+
+        end = {
+            date: date,
+            dateTime: dateTime,
+            timeZone: timeZone
+        }
+
         scheduledEvenets.push({
-            eventName: event.properties.Nome.title[0].plain_text,
-            allProperties: event.properties
+            summary: event.properties.Nome.title[0].plain_text,
+            start: start,
+            end: end,
+            allProperties: event.properties,
         })
     })
 
     return scheduledEvenets
 })
 
-preperaDataForCalendar().then((resolve) => console.log(resolve))
+
+
+preparateDataForCalendar().then((resolve) => console.log(resolve))
